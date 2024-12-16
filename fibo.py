6c19da3dcfe0e113e8c1ca81dfcf226a4ae36188
gkwjdtk09 <gkwjdtk@naver.com>
2024-12-16 12:04:38 +0900

    feat: Add fibo() with binet

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+#def fibo(num):
+#    if num < 2:
+#       return num
+#   else:
+#       return fibo(num-1) + fibo(num-2)
+from math import sqrt
+
 def fibo(num):
-    if num < 2:
-        return num
-    else:
-        return fibo(num-1) + fibo(num-2)
+    phi = (1 + sqrt(5)) / 2
+    psi = (1 - sqrt(5)) / 2
+
+    return round((phi**num - psi**num) /sqrt(5))
+
 print(fibo(5))
